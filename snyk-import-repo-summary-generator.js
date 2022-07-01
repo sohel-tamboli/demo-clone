8e9b705a33b4deb8c13976915aa3ddc5f5297874
Sohel Tamboli <sohel.tamboli@medly.com>
2022-07-01 11:04:57 +0530

    added summary geenrator

---
+++ snyk-import-repo-summary-generator.js
@@ -0,0 +1,32 @@
+import * as json from 'json';
+import * as core from '@actions/core';
+
+var FILENAME, import_job_file, import_time, imported, is_imported, json_log, repo_name, result_file;
+
+try {
+  FILENAME = "import-job-results.log";
+  import_job_file = open(FILENAME, "r");
+  result_file = open("result.txt", "w");
+
+  for (var log, _pj_c = 0, _pj_a = import_job_file, _pj_b = _pj_a.length; _pj_c < _pj_b; _pj_c += 1) {
+    log = _pj_a[_pj_c];
+    json_log = json.loads(log);
+
+    if (json_log["logs"].length > 0) {
+      repo_name = json_log["logs"][0]["name"];
+      import_time = json_log["logs"][0]["created"];
+
+      if (json_log["logs"][0]["projects"].length > 0) {
+        is_imported = json_log["logs"][0]["projects"][0]["success"];
+        imported = is_imported ? "successfully" : "not successfully";
+        result_file.write(`${repo_name} repository is ${imported} imported on ${import_time}`);
+        console.log(`${repo_name} repository is ${imported} imported on ${import_time} `);
+      }
+    }
+
+    await core.summary
+  .addHeading('Test Results').write()
+  }
+} catch (e) {
+  console.log(e);
+}
