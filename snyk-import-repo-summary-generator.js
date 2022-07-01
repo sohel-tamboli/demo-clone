b1e516e68b9d77eb1939969b7515f26f5f124b05
Sohel Tamboli <sohel.tamboli@medly.com>
2022-07-01 15:04:37 +0530

    updated

--- snyk-import-repo-summary-generator.js
+++ snyk-import-repo-summary-generator.js
@@ -1,36 +1,38 @@
 // import * as json from 'json';
 // import * as core from '@actions/core';
 
-const json = require('json')
-const core = require('@actions/core')
+// const core = require('@actions/core')
 var fs = require ('fs');
+const readline = require('readline');
+
 
 var FILENAME, import_job_file, import_time, imported, is_imported, json_log, repo_name, result_file;
 const summary_generator = async () =>{
 try {
   FILENAME = "import-job-results.log";
-  import_job_file = fs.readFileSync(FILENAME);
-  // result_file = fs.open("result.txt", "w");
+//   import_job_file = fs.readFileSync(FILENAME);
+  const readInterface = readline.createInterface({
+    input: fs.createReadStream(FILENAME),
+    console: false
+});
 
-  for (var log, _pj_c = 0, _pj_a = import_job_file, _pj_b = _pj_a.length; _pj_c < _pj_b; _pj_c += 1) {
-    log = _pj_a[_pj_c];
-    json_log = JSON.parse(log);
-
+readInterface.on('line', function(line) {
+    json_log = JSON.parse(line);
     if (json_log["logs"].length > 0) {
-      repo_name = json_log["logs"][0]["name"];
-      import_time = json_log["logs"][0]["created"];
-
-      if (json_log["logs"][0]["projects"].length > 0) {
-        is_imported = json_log["logs"][0]["projects"][0]["success"];
-        imported = is_imported ? "successfully" : "not successfully";
-        // result_file.write(`${repo_name} repository is ${imported} imported on ${import_time}`);
-        console.log(`${repo_name} repository is ${imported} imported on ${import_time} `);
-      }
+        repo_name = json_log["logs"][0]["name"];
+        import_time = json_log["logs"][0]["created"];
+  
+        if (json_log["logs"][0]["projects"].length > 0) {
+          is_imported = json_log["logs"][0]["projects"][0]["success"];
+          // result_file.write(`${repo_name} repository is ${imported} imported on ${import_time}`);
+          console.log(`${repo_name} repository is ${is_imported} imported on ${import_time} `);
+        }
     }
+});
 
     await core.summary
   .addHeading('Test Results').write()
-  }
+  
 } catch (e) {
   console.log(e);
 }
